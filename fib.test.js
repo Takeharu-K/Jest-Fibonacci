040489185436fe8222fbec35ec22f9b670d7ae9c
近藤岳晴 <takeharu.kondo@acompany-ac.com>
2020-03-27 20:06:58 +0900

    starting point

--- fib.test.js
+++ fib.test.js
@@ -1,9 +1,6 @@
 const fib = require('./fib');
 
-const arr = [[0, 0], [1, 1], [2, 1], [3, 2]];
 test('check Fibonacci' , () => {
-	for (let i = arr[0][0]; i < arr.length; i++){
-		expect(fib(arr[i][0])).toBe(arr[i][1]);
-	}
+
 });
 
